c8fd1c6b484eea53ccff5a40f5035f91d9524b70
linataataa <linataacha@gmail.com>
2023-12-25 21:34:45 +0100

    modification

--- healthIQ/src/main/java/com/healthIQ/healthIQ/Controllers/DoctorController.java
+++ healthIQ/src/main/java/com/healthIQ/healthIQ/Controllers/DoctorController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/doctor")
+
 public class DoctorController {
     @Autowired
     private DoctorService doctorService;
