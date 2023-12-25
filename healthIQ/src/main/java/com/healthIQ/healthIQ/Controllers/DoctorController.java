52099fdbf8c18c9860be29b5293930786a43fbd2
linataataa <linataacha@gmail.com>
2023-12-25 13:43:54 +0100

    Crud Doctor

---
+++ healthIQ/src/main/java/com/healthIQ/healthIQ/Controllers/DoctorController.java
@@ -0,0 +1,36 @@
+package com.healthIQ.healthIQ.Controllers;
+
+import com.healthIQ.healthIQ.Models.Doctor;
+import com.healthIQ.healthIQ.Services.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class DoctorController {
+    @Autowired
+    private DoctorService doctorService;
+
+    @GetMapping("/list")
+    public List<Doctor> getAllDoctors(){
+        return doctorService.getAllDoctors();
+    }
+    @GetMapping("/List/{id}")
+    public Doctor getDoctorById(@PathVariable String id){
+        return doctorService.getDoctorById(id);
+    }
+    @PostMapping("/add")
+    public Doctor addDoctor(@RequestBody Doctor doctor){
+        return doctorService.addDoctor(doctor);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void deleteDoctor(@PathVariable String id){
+        this.doctorService.deleteDoctor(id);
+    }
+    @PutMapping("/update/{id}")
+    public Doctor updateDoctor(@PathVariable String id, @RequestBody Doctor doctor){
+        return doctorService.updateDoctor(id,doctor);
+    }
+}
