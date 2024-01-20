60b64ed72f8786c2cbfc9ced60134eaa34b9efd5
MAmineBousbibit <mbousbibit@gmail.com>
2024-01-20 09:02:17 +0100

    notification SecurityConfig.java permission

--- healthIQ/src/main/java/com/healthIQ/healthIQ/Configurations/SecurityConfig.java
+++ healthIQ/src/main/java/com/healthIQ/healthIQ/Configurations/SecurityConfig.java
@@ -1,61 +1,62 @@
-    package com.healthIQ.healthIQ.Configurations;
+package com.healthIQ.healthIQ.Configurations;
 
-    import lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-    import org.springframework.context.annotation.Bean;
-    import org.springframework.context.annotation.Configuration;
-    import org.springframework.security.authentication.AuthenticationProvider;
-    import org.springframework.security.config.Customizer;
-    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-    import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-    import org.springframework.security.config.http.SessionCreationPolicy;
-    import org.springframework.security.web.SecurityFilterChain;
-    import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-    import org.springframework.web.cors.CorsConfiguration;
-    import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-    import org.springframework.web.filter.CorsFilter;
+import java.util.Arrays;
 
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
 
-    import java.util.Arrays;
-
-    @Configuration
-    @EnableWebSecurity
-    @RequiredArgsConstructor
-    public class SecurityConfig {
-
-        private final JwtAuthenticationFilter jwtAuthFilter;
-        private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
 
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-            http.csrf(AbstractHttpConfigurer::disable)
-                    .cors(Customizer.withDefaults())
-                    .authorizeHttpRequests(auth -> auth
-                            .requestMatchers("/api/v1/auth/**",
-                                            "/registrationConfirm",
-                                          "forgotPasswordRecovery"
-                                      , "/doctor/list" , "/doctor/add","/doctor/update/{id}","/doctor/delete/{id}").permitAll()
-                            .requestMatchers("").hasAnyRole("DOCTOR")
-                            .requestMatchers("").hasAnyRole("ADMIN")
-                            .anyRequest().authenticated())
-                    .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                    .authenticationProvider(authenticationProvider)
-                    .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-            ;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/**",
+                                "/registrationConfirm",
+                                "forgotPasswordRecovery"
+                                , "/doctor/list", "/doctor/add", "/doctor/update/{id}", "/doctor/delete/{id}"
+                                , "/api/v1/notification/**",
+                                "/api/v1/user/**").permitAll()
+                        .requestMatchers("").hasAnyRole("DOCTOR")
+                        .requestMatchers("").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
 
-            return http.build();
-        }
-        @Bean
-        public CorsFilter corsFilter(){
-              final UrlBasedCorsConfigurationSource source =new UrlBasedCorsConfigurationSource();
-              final CorsConfiguration config =new CorsConfiguration();
-              config.setAllowCredentials(true);
-              config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-              config.setAllowedHeaders(Arrays.asList("*"));
-              config.setAllowedMethods(Arrays.asList("*"));
-              source.registerCorsConfiguration("/**",config);
-              return new CorsFilter(source);
-        }
+        return http.build();
     }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
