736bbbb4f2aceee02c1e2455c8abce3ccc1b7d89
linataataa <linataacha@gmail.com>
2024-01-22 17:04:43 +0100

    lastupdate

--- healthIQ/src/main/java/com/healthIQ/healthIQ/Models/Doctor.java
+++ healthIQ/src/main/java/com/healthIQ/healthIQ/Models/Doctor.java
@@ -7,22 +7,23 @@ import lombok.Setter;
 import jakarta.persistence.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalTime;
+import java.util.Date;
+import java.util.List;
 
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "doctors")
+@Document(collection = "Users")
 public class Doctor {
     @Id
     private String id;
 
     private String first_name;
     private String last_name;
-    private LocalTime debutTime;
-    private LocalTime finTime;
+    private String debutTime;
+    private String finTime;
     private Role role;
 
     @Column(unique = true)
@@ -30,4 +31,11 @@ public class Doctor {
 
     @Column(unique = true)
     private String password;
+
+    private String adresse;
+    private int age;
+    private Double prixConsultation;
+    private String operation;
+    private String specialite;
+    private String dateNaissance;
 }
